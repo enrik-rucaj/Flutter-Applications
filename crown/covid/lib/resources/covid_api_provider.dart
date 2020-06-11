824a1a6ffd268f50ca6abd0c00af7ccb2afb65dc
enrik-rucaj <rucajenrik111@gmail.com>
2020-06-11 19:46:40 +0000

    Update crown/covid/lib/resources/covid_api_provider.dart

--- crown/covid/lib/resources/covid_api_provider.dart
+++ crown/covid/lib/resources/covid_api_provider.dart
@@ -1,34 +1,29 @@
 import 'dart:async';
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
-import '../models/item_model_country.dart';
-import '../models/item_model_continent.dart';
-import '../models/item_model_all.dart';
+import '../models/item_model_lista_criptovalute.dart';
+import '../models/item_model_storico_criptovaluta.dart';
+import '../models/item_model_criptovaluta.dart';
 
-class CovidApiProvider {
+
+class ApiProvider {
   Client client = Client();
+  const ultimiDati = 0;
+  int criptoValuta = 0;
 
-  Future<ItemModelCountry> fetchCountryList() async {
-    final response = await client.get("https://corona.lmao.ninja/v2/countries/");
+  Future<ItemModelListaCriptovalute> fetchCriptoValuteList() async {
+    final response = await client.get("https://www.mysite.it/api/criptovalute/");
     if (response.statusCode == 200) 
-      return ItemModelCountry.fromJson(json.decode(response.body));
+      return ItemModelListaCriptovalute.fromJson(json.decode(response.body));
     else 
       throw Exception('Failed to load post');
   }
 
-  Future<ItmeModelContinent> fetchContinentList() async {
-    final response = await client.get("https://corona.lmao.ninja/v2/continents/");
+  Future<ItmeModelCriptovaluta> fetchContinentList() async {
+    final response = await client.get("https://www.mysite.it/api/criptovalute/$criptoValuta/$ultimiDati");
     if (response.statusCode == 200)
-      return ItmeModelContinent.fromJson(json.decode(response.body));
+      return ItmeModelCriptovaluta.fromJson(json.decode(response.body));
     else
-      throw Exception('Failed to load post');
-  }
-
-  Future<ItemModelAll> fetchAllInfo() async {
-    final response = await client.get("https://corona.lmao.ninja/v2/all/");
-    if (response.statusCode == 200) 
-      return ItemModelAll.fromJson(json.decode(response.body));
-    else 
       throw Exception('Failed to load post');
   }
 }
